3f481a6b60d97a53c70c11471655c4dc19dc833a
Ssenkowski <scsenkowski@gmail.com>
2017-10-20 16:16:20 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,7 @@ def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 if position_taken?(board, location) == true
   counter += 1
-  until counter = 9
+  until counter == 9
     break
   end
 end

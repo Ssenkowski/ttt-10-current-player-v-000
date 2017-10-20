77fda9ffda90863eccf6a0ebfbeac593cae7be6c
Ssenkowski <scsenkowski@gmail.com>
2017-10-20 16:22:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,14 @@
 def turn_count(board)
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 counter = 0
-if turn_count(board) == true
+board.each do |board|
+  if board != " "
   counter += 1
   until counter == 9
     break
   end
 end
-end
+  
 
 
 def current_player

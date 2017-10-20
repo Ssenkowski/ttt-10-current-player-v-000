df2743ed69f2231400a4fa1b40c779720d749228
Ssenkowski <scsenkowski@gmail.com>
2017-10-20 17:14:46 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,8 @@ def turn_count(board)
 counter = 0
 board.each do |spaces|
   if spaces != board
-  counter += 1
+  return counter += 1
   until counter == 9
-    return == counter
     break
   end
 end
